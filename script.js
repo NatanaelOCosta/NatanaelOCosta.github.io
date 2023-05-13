1e8eadd3f53900aea64ead8866eb0cbeaea9c43e
Natanael Costa <natanael.ocosta@outlook.com>
2023-05-13 14:26:37 -0300

    Update

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function verificarHorario(){
 }
 
 window.addEventListener("load", verificarHorario);
+
+/*Funcao de rolagem suave de página*/
+document.querySelectorAll('a.smooth-scroll').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
